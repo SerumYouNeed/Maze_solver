dfb038eec0f2da21f78f2931809eaeadbc5e5a61
SerumYouNeed <greg.miler@gmail.com>
2024-09-26 19:01:58 +0200

    cell class added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from screen import Window, Point, Line, Cell
+from screen import Window, Point, Line
+from cell import Cell
 
 
 def main():
@@ -7,8 +8,8 @@ def main():
     # line = Line(Point(50,50), Point(400, 400))
     # screen.draw_line(line=line, fill_color="green")
 
-    c1 = Cell(True, True, True, True, 50, 50, 200, 200, screen)
-    c1.draw()
+    c1 = Cell(screen)
+    c1.draw(1500, 150, 200, 200)
 
     screen.wait_for_close()
     
--- screen.py
+++ screen.py
@@ -38,30 +38,3 @@ class Line:
 
     def draw(self, canvas, fill_color="black"):
         canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
-
-class Cell:
-    def __init__(self, has_left_wall, has_right_wall, has_top_wall, has_bottom_wall, x1, x2, y1, y2, win):
-        self.has_left_wall = has_left_wall
-        self.has_right_wall = has_right_wall
-        self.has_top_wall = has_top_wall
-        self.has_bottom_wall = has_bottom_wall
-        self._x1 = x1
-        self._x2 = x2
-        self._y1 = y1
-        self._y2 = y2
-        self._win = win
-
-    def draw(self):
-        if self.has_left_wall:
-            line = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
-            self._win.draw_line(line=line, fill_color="black")
-        if self.has_bottom_wall:
-            line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
-            self._win.draw_line(line, "black")
-        if self.has_top_wall:
-            line = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
-            self._win.draw_line(line, "black")
-        if self.has_right_wall:
-            line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
-            self._win.draw_line(line, "black")
-        
