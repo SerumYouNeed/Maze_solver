3e92dbed10c08a63c88da601b1226bee10acf63b
SerumYouNeed <greg.miler@gmail.com>
2024-10-01 16:25:08 +0200

    drawing cells inside maze

--- maze.py
+++ maze.py
@@ -1,4 +1,5 @@
 from cell import Cell
+from screen import Point
 
 class Maze:
     def __init__(self,
@@ -30,7 +31,19 @@ class Maze:
             self._cells.append(row)
         for i in self._cells:
             for j in i:
-                _draw_cells(i, j)
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i, j):
+        # start_point = Point(self.x, self.y)
+        # end_point = Point(self.x * self.)
+        # self.cell_size_x = i
+        # self.cell_size_y = j
+        x1 = self.x1 + j * self.cell_size_x
+        x2 = self.x1 + (j + 1) * self.cell_size_x
+        y1 = self.y1 + i * self.cell_size_y
+        y2 = self.y1 + (i + 1) * self.cell_size_y
+        cell = Cell(self.win)
+        cell.draw(x1, y1, x2, y2)
+        self._animate()
 
     
-        
