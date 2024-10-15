0a1a89bd3a47c758ba5fdc0558d8b174f895331d
SerumYouNeed <greg.miler@gmail.com>
2024-10-15 23:59:41 +0200

    breaking entry wall and exit wall function

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 from screen import Window, Point, Line
 from cell import Cell
+from maze import Maze
 
 
 def main():
-    screen = Window("500x500", "First window in class")
+    # screen = Window("500x500", "First window in class")
     
     # line = Line(Point(50,50), Point(400, 400))
     # screen.draw_line(line=line, fill_color="green")
 
-    c1 = Cell(screen)
-    c1.draw(150, 150, 200, 200)
-    c2 = Cell(screen)
-    c2.draw(300, 150, 350, 200)
-    c1.draw_move(c2, False)
+    # c1 = Cell(screen)
+    # c1.draw(150, 150, 200, 200)
+    # c2 = Cell(screen)
+    # c2.draw(300, 150, 350, 200)
+    # c1.draw_move(c2, False)
 
-    screen.wait_for_close()
+    # m = Maze(100, 100, 5, 5, 35, 25, screen)
+    # m._break_entrance_and_exit()
+    num_rows = 10
+    num_cols = 10
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window("800x600", "Maze")
+
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+
+    win.wait_for_close()
+    # screen.wait_for_close()
     
 
 
--- maze.py
+++ maze.py
@@ -1,49 +1,58 @@
 from cell import Cell
-from screen import Point
+import random
+import time
 
 class Maze:
-    def __init__(self,
-                 x1,
-                 y1,
-                 num_rows,
-                 num_cols,
-                 cell_size_x,
-                 cell_size_y,
-                 win,
-                 ):
-        self.x1 = x1
-        self.y1 = y1
-        self.num_rows = num_rows
-        self.num_cols = num_cols
-        self.cell_size_x = cell_size_x
-        self.cell_size_y = cell_size_y
-        self.win = win
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win=None,
+    ):
         self._cells = []
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
 
         self._create_cells()
+        self._break_entrance_and_exit()
 
     def _create_cells(self):
-        c = Cell(self.win)
-        for i in range(0, self.num_rows):
-            row = []
-            for j in range(0, self.num_cols):
-                row.append(c)
-            self._cells.append(row)
-        for i in self._cells:
-            for j in i:
+        for i in range(self._num_cols):
+            col_cells = []
+            for j in range(self._num_rows):
+                col_cells.append(Cell(self._win))
+            self._cells.append(col_cells)
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
                 self._draw_cell(i, j)
 
     def _draw_cell(self, i, j):
-        # start_point = Point(self.x, self.y)
-        # end_point = Point(self.x * self.)
-        # self.cell_size_x = i
-        # self.cell_size_y = j
-        x1 = self.x1 + j * self.cell_size_x
-        x2 = self.x1 + (j + 1) * self.cell_size_x
-        y1 = self.y1 + i * self.cell_size_y
-        y2 = self.y1 + (i + 1) * self.cell_size_y
-        cell = Cell(self.win)
-        cell.draw(x1, y1, x2, y2)
+        if self._win is None:
+            return
+        x1 = self._x1 + i * self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
         self._animate()
 
-    
+    def _animate(self):
+        if self._win is None:
+            return
+        self._win.redraw()
+        time.sleep(0.05)
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0, 0)
+        self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
+        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
